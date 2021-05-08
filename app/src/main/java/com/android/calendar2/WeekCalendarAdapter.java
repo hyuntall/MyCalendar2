7b571d5600706c8317ff6e519d8fc9d1f42f024a
leejh9811 <80316617+leejh9811@users.noreply.github.com>
2021-05-08 23:51:37 +0900

    커밋

--- app/src/main/java/com/android/calendar2/WeekCalendarAdapter.java
+++ app/src/main/java/com/android/calendar2/WeekCalendarAdapter.java
@@ -14,24 +14,18 @@ public class WeekCalendarAdapter extends FragmentStateAdapter {
     }
 
     int year = Calendar.getInstance().get(Calendar.YEAR);
-    int month = Calendar.getInstance().get(Calendar.MONTH)+1;
+    int month = Calendar.getInstance().get(Calendar.MONTH);
     int day;
-    int day2;
-    Calendar mCal;
-    int dm;
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        mCal = Calendar.getInstance();
         // year 변수에 저장된 값을 Year에, month변수에 저장된 값을 Month에, 1을 Day에 넣는다.
-        mCal.set(Integer.parseInt(String.valueOf(year)), Integer.parseInt(String.valueOf(month+dm)) - 1, 1);
-        int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
-        int dayMax = mCal.getActualMaximum(Calendar.DAY_OF_MONTH);
-        dm = (position*7)/42;
-        //System.out.println((position*7+1-dayNum) + "position" + dm + "dm" + dayNum + "dayNum" + dayMax + "dayMAx");
+        int dm = position*7/42;
+        int realMonth = (month+dm)%12+1;
+        int ym = (month+dm)/12;
         day = position;
-        return WeekCalendarFragment.newInstance(year, month+dm, day, day2);
+        return WeekCalendarFragment.newInstance((year+ym), realMonth, day);
     }
 
     @Override
