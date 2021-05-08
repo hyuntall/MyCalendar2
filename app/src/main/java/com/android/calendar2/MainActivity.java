d4250d9c85c98079727e7113e46ead4d0c2c2459
HyunTall <hyunchul1998@naver.com>
2021-05-08 21:55:06 +0900

    주석표시

--- app/src/main/java/com/android/calendar2/MainActivity.java
+++ app/src/main/java/com/android/calendar2/MainActivity.java
@@ -25,11 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View grid = getLayoutInflater().inflate(R.layout.fragment_month_calendar, null, false);
-        View item = getLayoutInflater().inflate(R.layout.item_month, null, false);
-        GridView gridview = (GridView)grid.findViewById(R.id.gridview);
-        TextView tv = (TextView)item.findViewById(R.id.item_gridview);
-        //System.out.println(tv.getWidth()+"가로입니다"+tv.getHeight());
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.main_container, new MonthViewFragment());
@@ -37,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) { // 액션바를 생성하는 코드이다.
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
@@ -45,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // 액션바에서 월간 달력 혹은 주간 달력을 선택할 수 있도록 해주는 코드이다.
         switch (item.getItemId()) {
             case R.id.month_view:
                 FragmentManager fragmentManager = getSupportFragmentManager();
